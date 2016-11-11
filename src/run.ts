cf5e4a9a8193e7a3afd078970cef96614d634630
Patrick Muff <muff.pa@gmail.com>
2016-11-11 18:22:17 +0100

    Make sudokus great again

--- src/run.ts
+++ src/run.ts
@@ -8,23 +8,25 @@ import { SudokuBacktracker } from './sudoku-backtracker';
 const printSudoku = (grid: number[][]) => {
   const getChar = (char) => {
     return char > 0 ? char.toString() : ' ';
-  }
+  };
+  const separatorLine = '+-------+-------+-------+'; 
   
   for (let x = 0; x < 9; x++) {
-    if (x === 0) {
-      console.log(Array(24).join('-'));
-    }
+    if (x === 0) console.log(separatorLine);
+
     let out = '';
     for (let y = 0; y < 9; y++) {
-      if ((y + 1) % 3 === 0 && y !== 0) {
+      if (y === 0) out += '| ';
+      if ((y + 1) % 3 === 0) {
         out += getChar(grid[x][y]) + ' | ';
       } else {
         out += getChar(grid[x][y]) + ' ';
       }
     }
     console.log(out);
+    
     if ((x + 1) % 3 === 0) {
-      console.log(Array(24).join('-'));
+      console.log(separatorLine);
     }
   }
 }
