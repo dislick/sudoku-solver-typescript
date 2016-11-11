359a8661117732979f19bcde6b951da5c4a3617b
Patrick Muff <muff.pa@gmail.com>
2016-11-11 15:51:56 +0100

    Add more tests for usedInRow() method

--- src/sudoku-solver.spec.ts
+++ src/sudoku-solver.spec.ts
@@ -4,14 +4,30 @@
 import { expect } from 'chai';
 import { SudokuBacktracker } from './sudoku-backtracker';
 
-describe('SudokuBacktracker', function() {
-  describe('.usedInRow()', function() {
-    it('should return true if the number is used in a given row', function() {
+describe('SudokuBacktracker', () => {
+  describe('.usedInRow()', () => {
+    it('should return true if the number is used in a given row', () => {
       let grid = [
-        [0,0,0,1,0,0,0,0,0]
+        [0,0,0, 1,0,0, 0,0,0]
       ];
       let backtracker = new SudokuBacktracker(grid);
       expect(backtracker.usedInRow(0, 1)).to.equal(true);
+    });
+
+    it('should return true if the number is used multiple times in a given row', () => {
+      let grid = [
+        [0,2,2, 0,0,0, 0,2,0]
+      ];
+      let backtracker = new SudokuBacktracker(grid);
+      expect(backtracker.usedInRow(0, 2)).to.equal(true);
+    });
+
+    it('should return false if the number is not used in a given row', () => {
+      let grid = [
+        [0,0,3, 2,0,1, 0,5,4]
+      ];
+      let backtracker = new SudokuBacktracker(grid);
+      expect(backtracker.usedInRow(0, 7)).to.equal(false);
     });
   });
 });
