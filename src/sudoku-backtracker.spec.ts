87b13e901c2a9d4f5da28967a675da312dc26ce9
Patrick Muff <muff.pa@gmail.com>
2016-11-13 16:51:31 +0100

    Add a unit test that actually tests if a sudoku can be solved

--- src/sudoku-backtracker.spec.ts
+++ src/sudoku-backtracker.spec.ts
@@ -106,4 +106,40 @@ describe('SudokuBacktracker', () => {
       expect(backtracker.usedInSquare(1, 1, 1)).to.equal(true);
     });
   });
+
+  describe('.solve()', () => {
+    it('should solve a test sudoku', () => {
+      // There is only one solution possible to this sudoku
+      let testSudoku = [
+        [2,0,0, 0,7,6, 0,4,8],
+        [0,0,0, 0,0,0, 2,0,9],
+        [0,0,0, 1,0,0, 0,0,0],
+
+        [0,0,4, 6,9,0, 0,0,0],
+        [9,2,0, 0,0,1, 3,0,0],
+        [6,0,0, 8,0,5, 0,0,0],
+
+        [1,4,7, 2,0,3, 0,5,6],
+        [8,9,0, 5,0,0, 1,0,0],
+        [0,0,3, 9,1,0, 8,0,7]
+      ];
+      let expectedOutput = [
+        [2,1,9, 3,7,6, 5,4,8],
+        [7,3,6, 4,5,8, 2,1,9],
+        [4,8,5, 1,2,9, 6,7,3],
+
+        [3,5,4, 6,9,2, 7,8,1],
+        [9,2,8, 7,4,1, 3,6,5],
+        [6,7,1, 8,3,5, 4,9,2],
+
+        [1,4,7, 2,8,3, 9,5,6],
+        [8,9,2, 5,6,7, 1,3,4],
+        [5,6,3, 9,1,4, 8,2,7]
+      ];
+
+      let backtracker = new SudokuBacktracker(testSudoku);
+      let output = backtracker.solve();
+      expect(output).to.eql(expectedOutput);
+    });
+  });
 });
